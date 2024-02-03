77cc6b0c9152103e0ec4875c7f7b10e378c50342
fzirhmn <fauzi.rahmn26@gmail.com>
2024-02-03 19:34:54 +0700

    Fix Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,9 +36,14 @@ class AuthController extends Controller
             return redirect('/masuk');
     }
 
-    public function logout(){
+    public function logout(Request $request){
 
         Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
         return redirect('/masuk');
 
     }
