8f6c4c8db1d3f8614ca41472a3de9abba2a44ada
fzirhmn <fauzi.rahmn26@gmail.com>
2024-02-03 20:05:57 +0700

    Revert "Fix Auth"
    This reverts commit 77cc6b0c9152103e0ec4875c7f7b10e378c50342.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,14 +36,9 @@ class AuthController extends Controller
             return redirect('/masuk');
     }
 
-    public function logout(Request $request){
+    public function logout(){
 
         Auth::logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
         return redirect('/masuk');
 
     }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
         'onlyAdmin' => OnlyAdmin::class,
     ];
 }
