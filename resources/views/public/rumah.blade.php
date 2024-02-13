82eaa5f3e1ed244c961bd6c764a80395e1944d7a
Daffa Muttaqin <nine@upi.edu>
2024-02-13 14:41:18 +0700

    Mengganti placeholder login siswa

--- resources/views/public/rumah.blade.php
+++ resources/views/public/rumah.blade.php
@@ -12,7 +12,7 @@
 
                 <div>
                     <label for="username" class="block mb-2 text-sm font-medium text-gray-900">NISN</label>
-                    <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" placeholder="00112233" required="">
+                    <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" placeholder="Masukkan NISN" required="">
                 </div>
                 <div>
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Kata Sandi</label>
