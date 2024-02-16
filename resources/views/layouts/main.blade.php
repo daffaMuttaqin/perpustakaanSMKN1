965cf96944e165b81fa88188f1a559ace37532e5
Daffa Muttaqin <nine@upi.edu>
2024-02-16 12:15:09 +0700

    Notifikasi Done

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -222,32 +222,54 @@
     </div>
 
 <!-- NOTIFIKASI MODAL -->
-<div id="notifikasi-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-lg max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                    Notifikasi
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="notifikasi-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <div class="p-4 md:p-5 space-y-4">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
-        </div>
-    </div>
+<div id="notifikasi-modal" data-modal-placement="top-right" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div class="relative w-full max-w-2xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <!-- Modal header -->
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                  Notifikasi
+              </h3>
+              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="notifikasi-modal">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+              </button>
+          </div>
+          <!-- Modal body -->
+          <div class="p-4 md:p-5 space-y-4">
+
+              <div class="grid grid-cols-8 gap-y-6 pb-4">
+                {{-- Notifikasi 1 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Dwi Annisa dari X TKJ 1 telah jatuh tempo untuk mengembalikan buku, tanggal peminjaman 10/11/2023
+                </div>
+
+                {{-- Notifikasi 2 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Kepala sekolah telah menyetujui Laporan Request Buku Baru yang anda ajukan
+                </div>
+
+                {{-- Notifikasi 3 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Dwi Annisa dari X TKJ 1 telah jatuh tempo untuk mengembalikan buku, tanggal peminjaman 10/11/2023
+                </div>
+
+              </div>
+
+          </div>
+      </div>
+  </div>
 </div>
     @include('partials.footer')
