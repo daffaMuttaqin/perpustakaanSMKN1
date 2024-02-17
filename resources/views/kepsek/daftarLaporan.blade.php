d15ae25f728ad03ed99256cadf0d67dc28108b42
Daffa Muttaqin <nine@upi.edu>
2024-02-17 18:46:24 +0700

    Page data laporan kepsek done

--- resources/views/kepsek/daftarLaporan.blade.php
+++ resources/views/kepsek/daftarLaporan.blade.php
@@ -1,7 +1,22 @@
-@extends('layouts.main')
-@section('konten')
+@extends('layouts.layout-kepsek')
+@section('konten-kepsek')
 {{-- KONTEN --}}
 <div class="w-full h-full">
+    <div class="flex items-center gap-x-24">
+        
+        {{-- Search Bar --}}
+        <form class="w-5/12 z-10">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input name="title" type="search" id="default-search" class="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-primary1" placeholder="Pencarian Data" required>
+            </div>
+        </form>
+    </div>
 
     {{-- TABEL --}}
     <div class="mt-5">
@@ -13,101 +28,115 @@
                             No
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Nama File
+                            NISN
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nama Anggota
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Judul Buku
                         </th>
                         <th scope="col" class="px-6 py-3 text-center">
-                            Ukuran File
+                            Tanggal Pinjam
                         </th>
                         <th scope="col" class="px-6 py-3 text-center">
-                            Tanggal Unggah
+                            Tanggal Kembali
                         </th>
                         <th scope="col" class="px-6 py-3 text-center">
-                            Aksi
+                            Denda
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-center">
+                            Status
                         </th>
                     </tr>
                 </thead>
+                {{-- <tbody>
+                    @forelse ($rents as $item)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                            {{ $loop->iteration }}
+                        </th>
+                        <td class="px-6">
+                            {{ $item->user->username }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $item->user->name }}
+                        </td>
+                        <td class="px-6 py-4 text-center">
+                            {{ $item->rentDate }}
+                        </td>
+                        <td class="px-6 py-4 text-center">
+                            {{ $item->returnDate }}
+                        </td>
+                        <td class="px-6 py-4 text-center">
+                            {{ $item->penalty }}
+                        </td>
+                        <td class="px-6 py-4 text-center">
+                            @if ($item->status == "Masih Dipinjam")
+                            <div class="px-5 py-1 text-sm text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
+                                Masih Dipinjam
+                            </div>
+                            @elseif ($item->status == "Sudah Kembali")
+                            <div class="px-5 py-1 text-sm text-green-600 bg-green-300 w-full rounded-full text-center font-medium hover:bg-green-400 duration-300">
+                                Sudah Kembali
+                            </div>
+                            @elseif ($item->status == "Terlambat")
+                            <div class="px-5 py-1 text-sm text-red-600 bg-red-300 w-full rounded-full text-center font-medium hover:bg-red-400 duration-300">
+                                Terlambat
+                            </div>
+                            @endif
+                        </td>
+                        <td class="flex px-6 py-4 text-center justify-center">
+                            <button data-modal-target="edit-status-modal{{ $item->id }}" data-modal-toggle="edit-status-modal{{ $item->id }}" type="button">
+                                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                                  </svg>      
+                            </button>
+                            <button data-modal-target="delete-modal{{ $item->id }}" data-modal-toggle="delete-modal{{ $item->id }}" type="button">
+                                <svg class="w-5 h-5 ml-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                            Data Tidak Ditemukan
+                        </th>
+                    </tr>
+                    
+                    @endforelse
+
+                </tbody> --}}
+
                 <tbody>
-                    {{-- Data 1 --}}
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                             1
                         </th>
-                        <td class="p-4">
-                            Laporan Denda Tahun 2023.pdf
+                        <td class="px-6">
+                            112131323
+                        </td>
+                        <td class="px-6 py-4">
+                            Andi
+                        </td>
+                        <td class="px-6 py-4">
+                            Pendidikan Agama Islam
                         </td>
                         <td class="px-6 py-4 text-center">
-                            910kb
+                            2/17/2024
                         </td>
                         <td class="px-6 py-4 text-center">
-                            10/26/2022
+                            2/18/2024
                         </td>
                         <td class="px-6 py-4 text-center">
-                            <button class="px-5 py-1 text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
-                                Lihat
-                            </button>
-                        </td>
-                    </tr>
-
-                    {{-- Data 2 --}}
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                            2
-                        </th>
-                        <td class="p-4">
-                            Laporan Denda Tahun 2023.pdf
+                            Rp. 25.000
                         </td>
                         <td class="px-6 py-4 text-center">
-                            910kb
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            10/26/2022
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            <button class="px-5 py-1 text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
-                                Lihat
-                            </button>
-                        </td>
-                    </tr>
-
-                    {{-- Data 3 --}}
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                            3
-                        </th>
-                        <td class="p-4">
-                            Laporan Denda Tahun 2023.pdf
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            910kb
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            10/26/2022
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            <button class="px-5 py-1 text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
-                                Lihat
-                            </button>
-                        </td>
-                    </tr>
-
-                    {{-- Data 4 --}}
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="w-4 p-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                            4
-                        </th>
-                        <td class="p-4">
-                            Laporan Denda Tahun 2023.pdf
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            910kb
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            10/26/2022
-                        </td>
-                        <td class="px-6 py-4 text-center">
-                            <button class="px-5 py-1 text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
-                                Lihat
-                            </button>
+                            <div class="px-5 py-1 text-sm text-blue-600 bg-blue-300 w-full rounded-full text-center font-medium hover:bg-blue-400 duration-300">
+                                Masih Dipinjam
+                            </div>
                         </td>
                     </tr>
 
@@ -116,5 +145,6 @@
         </div>
     </div>
 </div>
+
 {{-- END KONTEN --}}
 @endsection
---
+++ resources/views/layouts/layout-kepsek.blade.php
@@ -0,0 +1,252 @@
+{{-- Header --}}
+@include('partials.header')
+    
+<!-- Navbar -->
+<nav class="flex fixed z-40 h-22 w-full px-7 bg-primary1 justify-between shadow-xl inset-0">
+  <!-- Bagian Kiri Logo dan Nama Sekolah -->
+  <div class="flex items-center">
+    <!-- Logo -->
+    <a href="/" class="hover:scale-110 duration-300">
+      <!-- Logo -->
+      <img
+          src="img/icon.png"
+          alt=""
+      />
+    </a>
+    <!-- Nama Sekolah -->
+    <div class="font-bold ml-7 text-white text-xl">
+      Perpustakaan SMK N1 Bumi Ratu Nuban
+    </div>
+  </div>
+
+  <!-- Bagian Kanan Notifikasi dan Avatar -->
+  <div class="flex items-center">
+    <!-- Icon Notifikasi -->
+    <button data-modal-target="notifikasi-modal" data-modal-toggle="notifikasi-modal" type="button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-10 h-10 hover:scale-110 duration-100 text-white mx-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+    </button>
+
+    <!-- Avatar Profile -->
+    <div>
+      <img
+        id="avatarButton"
+        type="button"
+        data-dropdown-toggle="userDropdown"
+        data-dropdown-placement="bottom-start"
+        class="w-12 h-12 rounded-full cursor-pointer"
+        src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}"
+        alt="User dropdown"
+      />
+
+      <!-- Dropdown menu -->
+      <div
+        id="userDropdown"
+        class="z-10 w-1/4 hidden bg-white divide-y divide-gray-100 rounded-lg shadow"
+      >
+        <div
+          class="px-4 py-3 text-sm text-gray-900 text-center justify-center"
+        >
+          <div class="font-normal text-3xl py-2">Profil</div>
+          <hr />
+          <!-- Avatar didalam Drop Down -->
+          <img class="w-28 h-28 mx-auto mt-5 mb-3 rounded-full" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar"
+          />
+          <!-- Nama & Icon Edit -->
+          <div class="flex w-full py-1 justify-center">
+            <div class="font-medium truncate px-2">{{ Auth::user()->name }}</div>
+            {{-- Button Edit --}}
+            <a href="/dataPekerja"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+              </svg>
+            </a>
+          </div>
+          <!-- Badge Admin -->
+          <div class="py-2">
+            <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
+              >{{ Auth::user()->role }}</span>
+          </div>
+          <hr />
+          <!-- NIK -->
+          <div class="py-2">{{ Auth::user()->nip }}</div>
+          <hr />
+          <!-- Nomor Telpon -->
+          <div class="py-2">Nomor Telpon : {{ Auth::user()->phone }}</div>
+          <hr />
+          <!-- Tombol Keluar -->
+          <div class="py-2">
+            <a href="/keluar" class="bg-primary1 hover:bg-blue-800 duration-300 text-white text-center px-6 py-1 rounded-full">
+              Keluar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+    <div class="h-22 w-full bg-primary1"></div>
+    <div class="h-22 w-full bg-primary1">
+      <div class="flex h-full items-center">
+        <!-- Void untuk menggeser Judul ke Kanan -->
+        <div class="w-1/5"></div>
+        <!-- Judul -->
+        <div class="mt-4 text-white font-medium text-3xl"> {{ $title }}</div>
+      </div>
+    </div>
+
+    <!-- SIDE BAR -->
+    <div class="fixed top-32 left-4 w-2/12 bg-white h-4/5 rounded-xl shadow-lg">
+        <div class="grid grid-rows-4 grid-flow-col gap-4 h-full w-full">
+            {{-- Avatar Sidebar --}}
+            <div>
+                <img
+                    class="w-16 h-16 mx-auto mt-16 mb-3 rounded-full"
+                    src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}"
+                    alt="Rounded avatar"
+                />
+                <!-- Nama & Icon Edit -->
+                <div class="flex w-full mt-1 justify-center">
+                    <div class="font-bold truncate px-2">{{ Auth::user()->name }}</div>
+                </div>
+                <!-- Badge Admin -->
+                <div class="mt-1 text-center">
+                    <span class="bg-green-100 text-green-800 text-xs font-base px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{{ Auth::user()->role }}</span>
+                </div>
+            </div>
+            
+            {{-- Menu Sidebar --}}
+            <div class="row-span-2 mt-9">
+                <div class="flex flex-col">
+                    {{-- Tombol Sedang aktif --}}
+                    <a href="/perpustakaan" class="flex bg-bgSubJudul py-1 mx-3 rounded-full">
+                        <svg class="w-6 h-6 ml-7 mr-2 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4"/>
+                        </svg>
+                        <div class="text-primary1 font-semibold">Perpustakaan</div>
+                    </a>
+                    {{-- End Tombol Sedang Aktif --}}
+
+                    <a href="/kepsek" class="flex py-1 mx-3 rounded-full hover:bg-bgSubJudul hover:font-semibold duration-100">
+                        <svg class="w-6 h-6 ml-7 mr-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.828 10h6.239m-6.239 4h6.239M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
+                          </svg>
+                        <div class="">Laporan</div>
+                    </a>
+                </div>
+            </div>
+
+            {{-- Tombol Keluar Sidebar --}}
+            <div class="flex items-end mb-4">
+                <a href="/keluar" class="w-1/2 h-max py-1 mx-auto text-white text-center bg-primary1 rounded-full hover:bg-blue-800 duration-300">Keluar </a>
+            </div>
+            {{-- End Tombol Keluar --}}
+        </div>
+    </div>
+
+    <div style="height: 78%;" class="flex w-full">
+      <!-- Void untuk menggeser konten ke kanan -->
+      <div class="h-full w-1/5"></div>
+
+      <div class="w-4/5 pr-14">
+          <!-- Sub Judul -->
+          <div class="flex mt-4">
+            {{-- Class Sub Judul Aktif --}}
+            @if ($subJudul == '')
+            @else
+            <a href="{{ route($subJudul) }}" class="font-medium text-base text-primary1 px-5 py-1 rounded-full bg-bgSubJudul">
+              {{ $subJudul }}
+            </a>
+            @endif
+            
+            {{-- Class Sub Judul Normal --}}
+            @if ($subJudul2 == '')
+            @else
+            <a href="{{ route($subJudul2) }}" class="font-medium text-base text-fontSubJudul px-5 py-1 ml-11">
+              {{ $subJudul2 }}
+            </a>
+            @endif
+
+            {{-- Class Sub Judul Normal --}}
+            @if ($subJudul3 == '')
+            @else
+            <a href="{{ route($subJudul3) }}" class="font-medium text-base text-fontSubJudul px-5 py-1 ml-11">
+              {{ $subJudul3 }}
+            </a>
+            @endif
+
+          </div>
+          {{-- End Sub Judul --}}
+
+          {{-- KONTEN --}}
+          <div  class="w-full mt-5 p-7 bg-white rounded-xl shadow-lg">
+            @yield('konten-kepsek')
+          </div>
+          {{-- END KONTEN --}}
+          <div class="pt-10">
+          </div>
+      </div>
+    </div>
+
+<!-- NOTIFIKASI MODAL -->
+<div id="notifikasi-modal" data-modal-placement="top-right" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div class="relative w-full max-w-2xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <!-- Modal header -->
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                  Notifikasi
+              </h3>
+              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="notifikasi-modal">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+              </button>
+          </div>
+          <!-- Modal body -->
+          <div class="p-4 md:p-5 space-y-4">
+
+              <div class="grid grid-cols-8 gap-y-6 pb-4">
+                {{-- Notifikasi 1 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Dwi Annisa dari X TKJ 1 telah jatuh tempo untuk mengembalikan buku, tanggal peminjaman 10/11/2023
+                </div>
+
+                {{-- Notifikasi 2 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Kepala sekolah telah menyetujui Laporan Request Buku Baru yang anda ajukan
+                </div>
+
+                {{-- Notifikasi 3 --}}
+                {{-- Avatar --}}
+                <img class="w-12 h-12 rounded-full mx-auto col-span-1" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}" alt="Rounded avatar">
+                {{-- Teks Notifikasi --}}
+                <div class="col-span-7">
+                  Dwi Annisa dari X TKJ 1 telah jatuh tempo untuk mengembalikan buku, tanggal peminjaman 10/11/2023
+                </div>
+
+              </div>
+
+          </div>
+      </div>
+  </div>
+</div>
+    @include('partials.footer')
