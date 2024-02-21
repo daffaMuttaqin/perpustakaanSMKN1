2b4ffaa96947801c151312bfda7c2a23a75ca55a
fzirhmn <fauzi.rahmn26@gmail.com>
2024-02-21 20:27:47 +0700

    Fix Length Input Password

--- resources/views/public/daftar.blade.php
+++ resources/views/public/daftar.blade.php
@@ -68,11 +68,11 @@
                 </div>
                 <div>
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" required>
+                    <input type="password" name="password" id="password" minlength="8" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" required>
                 </div>
                 <div>
                     <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Konfirmasi Kata Sandi</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" required>
+                    <input type="password" name="password_confirmation" minlength="8" id="password_confirmation" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary1 focus:border-primary1 block w-full p-2.5" required>
                 </div>
                 <button type="submit" class="w-full text-white bg-primary1 hover:bg-blue-800 duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Buat Akun</button>
                 <p class="text-sm font-light text-gray-500">
